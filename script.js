d5ecb78693066cdbd08a643d69b75c8340a3f2d3
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2021-07-20 17:21:31 +0200

    second commit using row.splice for last step

--- script.js
+++ script.js
@@ -24,16 +24,21 @@ const isEven = () => {
   while (row.length < 9) {
     rando = randomNumber();
     if (!row.includes(rando) && row.length === 8 && rando === row[5] + 1) {
-      console.log(`${row[0]} ${row[1]} ${row[2]}
-${row[3]} ${row[4]} ${row[5]}
-${row[6]} ${row[7]} ${rando} (9) is an increment of (8)`);
+      console.log(`${row[0]} ${row[1]} ${row[2]}`);
+      console.log(`${row[3]} ${row[4]} ${row[5]}`);
+      console.log(`${row[6]} ${row[7]} ${rando}`);
+      console.log(`(9) ${rando} is an increment of (8) ${row[5]}`);
 
-      row = [];
+      const removed = row.splice(5, 1);
+      row.push(rando);
+
+      removed[0] !== row[5] + 1 ? row.push(removed[0]) : row.pop();
+      // console.log(`'removed' is an increment of '[5] + 1', loop again`);
     } else if (!row.includes(rando) && rando !== row[row.length - 3] + 1) {
       row.push(rando);
       console.log(row, rando);
     } else if (!row.includes(rando) && rando === row[row.length - 3] + 1) {
-      console.log(row, rando, `Increment detected`);
+      console.log(row, rando, `Increment detected of ${row[row.length - 3]}`);
     } else {
       console.log(`... amount of retries`);
       // console.log(row, rando, `is the same value as a row value`);
